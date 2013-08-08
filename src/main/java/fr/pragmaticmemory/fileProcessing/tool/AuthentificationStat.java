b750034d92830a1957ded6f0408f303aa9380e8d
CIGNETT <externe.cedric.cignetti@allianzgi.fr>
2013-08-08 14:36:50 +0200

    Amelioration des statistiques d'authentification.

--- src/main/java/fr/pragmaticmemory/fileProcessing/tool/AuthentificationStat.java
+++ src/main/java/fr/pragmaticmemory/fileProcessing/tool/AuthentificationStat.java
@@ -9,10 +9,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+/*
+    Input : log serveur
+ */
 public class AuthentificationStat {
 
-    private static final String USER = "USER_BATCH";
-//        private static final String USER="\\w+";
+//            private static final String USER = "USER_BATCH";
+    private static final String USER = "cignett";
+//    private static final String USER = "\\w+";
 
 
     public static void main(String[] args) throws Exception {
@@ -35,11 +39,15 @@ public class AuthentificationStat {
 
         private List<String> startLines = new ArrayList<String>();
         private List<String> endLines = new ArrayList<String>();
+        private List<Boolean> isLoginAccepted = new ArrayList<Boolean>();
         private List<String> outputLines = new ArrayList<String>();
+        //                static private final Pattern START_PATTERN = Pattern.compile("Adding node <country_of_" + USER + "_\\w+> to the platform");
         static private final Pattern START_PATTERN = Pattern.compile(
-              "Adding node <country_of_" + USER + "_\\w+> to the platform");
-        static private final Pattern END_PATTERN = Pattern.compile("Login '" + USER + "' accepted");
-        static private final Pattern CLEAN_PATTERN = Pattern.compile("(\\s*\\d+ \\w+ \\d+ \\d+:\\d+:\\d+,\\d+).+");
+              "AbstractSecurityServiceHelper  - Trying to login with \\(user: " + USER + ", securityLevel: \\w+\\)");
+
+        static private final Pattern LOGIN_ACCEPTED_PATTERN = Pattern.compile("Login '" + USER + "' accepted");
+        static private final Pattern LOGIN_REFUSED_PATTERN = Pattern.compile("Login '" + USER + "' refused");
+        static private final Pattern DATE_TIME_PATTERN = Pattern.compile("(\\s*\\d+ \\w+ \\d+ \\d+:\\d+:\\d+,\\d+).+");
 
 
         @Override
@@ -48,33 +56,45 @@ public class AuthentificationStat {
                 Matcher startMatcher = START_PATTERN.matcher(inputLine);
                 if (startMatcher.find()) {
                     startLines.add(inputLine);
-
                     continue;
                 }
-                Matcher endMatcher = END_PATTERN.matcher(inputLine);
-                if (endMatcher.find()) {
+                Matcher loginAcceptedMatcher = LOGIN_ACCEPTED_PATTERN.matcher(inputLine);
+                if (loginAcceptedMatcher.find()) {
                     endLines.add(inputLine);
+                    isLoginAccepted.add(true);
+                }
+                Matcher loginRefusedMatcher = LOGIN_REFUSED_PATTERN.matcher(inputLine);
+                if (loginRefusedMatcher.find()) {
+                    endLines.add(inputLine);
+                    isLoginAccepted.add(false);
                 }
             }
 
-            outputLines.add("Start :");
-            for (String startLine : startLines) {
-                outputLines.add(startLine);
-            }
-            outputLines.add("End :");
-            for (String endLine : endLines) {
-                outputLines.add(endLine);
-            }
-
-            clean(outputLines);
+            generateOutputLines();
             return outputLines;
+        }
+
+
+        private void generateOutputLines() {
+            clean(startLines);
+            clean(endLines);
+
+            for (int i = 0, startLinesSize = endLines.size(); i < startLinesSize; i++) {
+                StringBuilder builder = new StringBuilder();
+                if (isLoginAccepted.get(i)) {
+                    builder.append(startLines.get(i));
+                    builder.append("|");
+                    builder.append(endLines.get(i));
+                    outputLines.add(builder.toString());
+                }
+            }
         }
 
 
         private void clean(List<String> lines) {
             for (int i = 0, linesSize = lines.size(); i < linesSize; i++) {
                 String line = lines.get(i);
-                Matcher cleanMatcher = CLEAN_PATTERN.matcher(line);
+                Matcher cleanMatcher = DATE_TIME_PATTERN.matcher(line);
                 lines.set(i, cleanMatcher.replaceAll("$1"));
             }
         }
--- src/main/java/fr/pragmaticmemory/fileProcessing/tool/RequestTimeout.java
+++ src/main/java/fr/pragmaticmemory/fileProcessing/tool/RequestTimeout.java
@@ -1,6 +1,64 @@
 package fr.pragmaticmemory.fileProcessing.tool;
+import fr.pragmaticmemory.fileProcessing.core.Processor;
+import fr.pragmaticmemory.fileProcessing.core.RouteProvider;
+import fr.pragmaticmemory.fileProcessing.fileProvider.SingleFileProvider;
+import fr.pragmaticmemory.fileProcessing.processor.TextProcessor;
+import fr.pragmaticmemory.fileProcessing.routeProvider.MappedFileRouteProvider;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /**
- *
+ * Liste les handler dont l'exécution dépasse la minute à partir des logs serveurs
  */
 public class RequestTimeout {
+    static final Pattern TIMEOUT_PATTERN = Pattern.compile(
+          "(\\d{2}\\s\\w{3}\\s\\d{4}\\s\\d{2}:\\d{2}:\\d{2},\\d{3}).*Traitement de la requete en (\\d+)\\s+ms");
+    static final Pattern HANDLER_PATTERN = Pattern.compile("Execution du handler (\\w+)");
+
+
+    public static void main(String[] args) throws Exception {
+        String inputFileName = "C:\\Users\\cignett\\Desktop\\Nouveau dossier\\ServeurLog.txt";
+        String outputFileName = "C:\\Users\\cignett\\Desktop\\Nouveau dossier\\Stat.txt";
+
+        SingleFileProvider inputFileProvider = new SingleFileProvider(inputFileName);
+        SingleFileProvider outputFileProvider = new SingleFileProvider(outputFileName);
+        RouteProvider routeProvider = new MappedFileRouteProvider(inputFileProvider, outputFileProvider);
+        Processor processor = new RequestTimeoutProcessor();
+        processor.process(routeProvider);
+    }
+
+
+    static private class RequestTimeoutProcessor extends TextProcessor {
+
+        private String handler = "";
+
+
+        @Override
+        protected List<String> processFileContent(List<String> inputLines) throws Exception {
+            List<String> output = new ArrayList<String>();
+            for (int lineIndex = 1, inputLinesSize = inputLines.size(); lineIndex < inputLinesSize; lineIndex++) {
+                String line = inputLines.get(lineIndex);
+                Matcher handlerMatcher = HANDLER_PATTERN.matcher(line);
+                if (handlerMatcher.find()) {
+                    handler = handlerMatcher.group(1);
+                }
+                Matcher timeoutMatcher = TIMEOUT_PATTERN.matcher(line);
+                if (timeoutMatcher.find()) {
+                    StringBuilder builder = new StringBuilder();
+                    String time = timeoutMatcher.group(1);
+                    int duration = Integer.parseInt(timeoutMatcher.group(2));
+                    if (duration > 60000) {
+                        builder.append(time);
+                        builder.append("|");
+                        builder.append(handler);
+                        builder.append("|");
+                        builder.append(duration);
+                        output.add(builder.toString());
+                    }
+                }
+            }
+            return output;
+        }
+    }
 }
